636e75ba83ede3725e847dbdfbca44d541347927
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-06-01 19:20:31 +0530

    Add files via upload

---
+++ Sender/Sender.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include "Sender_Header.h"
+
+float Temperature[MAX_VAL] = {};
+float ChargeRate[MAX_VAL] = {};
+int filevalue = 0;
+int index = 0;
+
+Sender_State (*SenderDataRead[])(float Temperature[],float ChargeRate[])={ReadData};
+Sender_State(*SenderOutput[])(float Temperature[],float ChargeRate[])={outputtoconsole};
+
+Sender_State ReadData(float Temperature[],float ChargeRate[])
+{
+
+    float TempVal,ChargeRateVal;
+
+    Sender_State ReadStatus= FAIL;
+    FILE * file=fopen( "Sender_Text.txt" , "r");
+
+    if (file) {
+        for(int count=0;fscanf(file, "%f\t\t%f\n", &TempVal,&ChargeRateVal)!=EOF ;count++)
+        {
+          
+            Temperature[count]=TempVal;
+            ChargeRate[count]=ChargeRateVal;
+        }
+
+
+        ReadStatus= PASS;
+    }
+	
+	else {
+		
+		printf("\t\t**DATA READ FAILURE**\t\t\n");
+		ReadStatus= FAIL;
+	}
+    fclose(file);
+    return ReadStatus;
+
+}
+
+
+Sender_State inputvalue(Sender_InType inputvalue)
+{
+
+    Sender_State FileReadSuccess = FAIL;
+    FileReadSuccess=(*SenderDataRead[inputvalue])(Temperature,ChargeRate);
+    return FileReadSuccess;
+
+
+}
+
+Sender_State outputvalue(Sender_OutType outputvalue)
+{
+
+    Sender_State WriteStatus = FAIL;
+    WriteStatus=(*SenderOutput[outputvalue])(Temperature,ChargeRate);
+    return WriteStatus;
+
+}
+
+Sender_State outputtoconsole(float Temperature[],float ChargeRate[])
+{
+    for(int index=0;index<20;index++)
+    {
+        printf("Temperature value == %f \t ChargeRate value == %f \t\n",Temperature[index],ChargeRate[index]);
+    }
+    return PASS;
+
+
+}
