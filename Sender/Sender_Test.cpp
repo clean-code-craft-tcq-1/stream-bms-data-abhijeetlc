e6cda15877580d06c3053057956ccac2623fbbde
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-06-02 13:40:23 +0530

    Create Sender_Test.cpp

---
+++ Sender/Sender_Test.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "Sender_Header.h"
+
+TEST_CASE("To check if file was successfully opened to read") 
+{
+ 
+  REQUIRE(ReadData()== Success); 
+}
+
+TEST_CASE("To check data is printed on the console") 
+{
+  REQUIRE(outputtoconsole(UserRequest)== Success);
+  
+}
