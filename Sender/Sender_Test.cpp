ff4483bd5a0380a20151ed91843e698452aa440d
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-06-02 16:12:35 +0530

    Update Sender_Test.cpp

--- Sender/Sender_Test.cpp
+++ Sender/Sender_Test.cpp
@@ -5,13 +5,13 @@
 
 TEST_CASE("To check if file was successfully opened to read") 
 {
-  Sender_InType inputvalue = FileInput ; 
-  REQUIRE(inputvalue(inputvalue)== PASS); 
+  Sender_InType InVal = FileInput ; 
+  REQUIRE(InputValue(InVal)== PASS); 
 }
 
 TEST_CASE("test") 
 {
-  Sender_OutType outputvalue = printtoconsole ;
-  REQUIRE(outputvalue(outputvalue)== PASS);
+  Sender_OutType OutVal = PrintToConsole ;
+  REQUIRE(OutputValue(OutVal)== PASS);
   
 }
