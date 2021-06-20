59c4ad04a77e52066b37bfb77d5194296ec9db5d
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-06-20 12:21:47 +0530

    Update receive.h

--- Receiver/receive.h
+++ Receiver/receive.h
@@ -1,1 +1,25 @@
 
+#define NoOfParamterSet 20 
+#define lengthofParm 15 // As reference from the sender
+
+#define TemperatureMax  45.f
+#define TemperatureMin  0.f
+
+#define SocMAX  0.8f
+#define SOcMin  0.f
+
+typedef enum MinMaxRet
+{
+  Temperature,
+  SOC
+}MinMaxRet;
+
+
+void calculateMaxValue(float *paramValue, float *paramValueMax);
+void calculateMinValue(float *paramValue, float *paramValueMin);
+
+void Receiver_Read();
+bool readDataFromConsole(char *valuefromsender);
+void getvalueFromsender(char *valuefromsender_get, float *tempValue);
+
+
