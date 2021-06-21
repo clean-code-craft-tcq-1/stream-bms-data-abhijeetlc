7210200be3e6c37a1b9bcbad6c8ea35bc94a0edc
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-06-21 11:43:43 +0530

    Update receiver.c

--- Receiver/receiver.c
+++ Receiver/receiver.c
@@ -20,16 +20,35 @@ bool  readDataFromConsole(char *valuefromsender) {
 
 void getvalueFromsender(char *valuefromsender_get, float *tempValue)
 {
-  int i = 0, j = 0; 
-  char par[lengthofParm]; 
-  int paramvalue = 0;  
-  while ((valuefromsender_get[i] != '\n')){  
+  
+   int i = 0, j = 0; //index counter
+
+  char par[lengthofParm]; //string for each parameter 
+  int parNum = 0;  //count of parameters
+
+  /* Read till we reach end of string */
+  while ((valuefromsender_get[i] != '\n')){
+    
+    /* Read each parameter */
+    for (j = 0;valuefromsender_get[i] != NULL;j++, i++)
+    {
       par[j] = valuefromsender_get[i];
-      par[j] = '\0'; 
-    tempValue[paramvalue] = atof(par);
-    printf("Paramter value = %f\n", tempValue[paramvalue]); //for debug purpose
-    paramvalue++; 
+    }
+
+    par[j] = '\0'; //Adding end of string
+    
+    /* Convert string to float number to evaluate further */
+    tempValue[parNum] = atof(par);
+    
+    printf("Paramter value = %f\n", tempValue[parNum]); //for debug purpose
+
+    /* Iterate to next parameter in the buffer*/ 
+    parNum++; 
+
+    /*Escape Comma separated character*/
     i++;
+    
+  
   }
 }
 
