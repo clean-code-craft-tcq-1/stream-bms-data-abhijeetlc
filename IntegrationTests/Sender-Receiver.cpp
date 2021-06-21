94844d78e3c23a248ac44c4cb1266b8abe1f45af
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-06-21 13:01:38 +0530

    Update Sender-Receiver.cpp

--- IntegrationTests/Sender-Receiver.cpp
+++ IntegrationTests/Sender-Receiver.cpp
@@ -1,1 +1,27 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "../test/catch.hpp"
+#include "../Receiver/Receiver.h"
+#include "stddef.h"
+#include "string.h"
+
+
+TEST_CASE("Test case to test the Maximum Value of temperture in range")
+{
+  
+}
+
+TEST_CASE("Test case to test the Maximum Value of SOC in range")
+{
+
+}
+
+TEST_CASE("Test case to test the Minimum Value of temperture in range")
+{
+
+}
+
+TEST_CASE("Test case to test the Maximum Value of SOC in range")
+{
+
+}
